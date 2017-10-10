8e33c396ae23116945c6d866cdda929a6b0e1f14
Thomas Vivien <thomas.vivien@free.fr>
2017-10-10 06:16:26 +0200

    Update h237.js
    Hides with css rather than javascript: avoid blinking on page

--- h237.js
+++ h237.js
@@ -2,7 +2,7 @@
 // @name         h237
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  h237 antiadd killer
+// @description  h237 killer
 // @author       Monk
 // @match        https://www.protect-lien.com/*
 // @match        https://www.protect-zt.com/*
@@ -11,16 +11,8 @@
 (function() {
     'use strict';
 
-    var interval = setInterval(function () {
-        var h237 = document.getElementById('h237');
-
-        console.log('test...');
-        if (h237) {
-            h237.setAttribute('style', 'display: none;');
-            document.querySelector('.content').setAttribute('style', 'display: block;');
-
-            console.log('cleared');
-            clearInterval(interval);
-        }
-    }, 50);
+    $('body').append('<style>' + [
+        '#h237, .content center {display: none !important}',
+        '#h237 ~ .content {display: inherit !important}'
+    ].join(' ') + '</style>');
 })();
