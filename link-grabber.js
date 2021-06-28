63d70d4b19bca81b47e0e84ba95ced9963e695dc
Thomas Vivien <thomas.vivien.yang@pm.me>
2021-06-28 22:57:45 +0200

    feat: add style and tools
    Add some style with Tailwind
    Add a copy button
    Add a pattern input

--- link-grabber.js
+++ link-grabber.js
@@ -22,6 +22,13 @@
     return;
   }
 
+  // Install TailwindCSS
+  // <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" type="text/css">
+  const tailwindLinkElement = document.createElement('link');
+  tailwindLinkElement.href = 'https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css';
+  tailwindLinkElement.rel = 'stylesheet';
+  document.head.appendChild(tailwindLinkElement);
+
   const urls = [];
 
   // Get previous urls from localStorage
@@ -41,18 +48,64 @@
 
   const alertElement = document.querySelector('.content-body .alert');
 
+  // Create reset button
+  const resetButtonElement = document.createElement('div');
+  resetButtonElement.className = 'cursor-pointer rounded border border-black border-red-600 hover:border-red-400 p-1 text-red-600 hover:text-red-400';
+  resetButtonElement.innerHTML = '<svg class="inline" style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" /></svg> Clear';
+  resetButtonElement.addEventListener('click', () => {
+    localStorage.removeItem('urls');
+    writeListContent();
+    actionWrapperElement.style.display = listElement.style.display = 'none';
+  });
+
+  // Create copy button
+  const copyButtonElement = document.createElement('div');
+  copyButtonElement.className = 'cursor-pointer rounded border border-black border-blue-600 hover:border-blue-400 p-1 text-blue-600 hover:text-blue-400';
+  copyButtonElement.innerHTML = '<svg class="inline" style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M19,21H8V7H19M19,5H8A2,2 0 0,0 6,7V21A2,2 0 0,0 8,23H19A2,2 0 0,0 21,21V7A2,2 0 0,0 19,5M16,1H4A2,2 0 0,0 2,3V17H4V3H16V1Z" /></svg> Copy';
+  copyButtonElement.addEventListener('click', () => {
+    const content = urls.map((url) => {
+      if (!prefixInputElement.value) {
+        return url;
+      }
+
+      return `${prefixInputElement.value.replace('[url]', url)}`;
+    }).join('\r\n');
+
+    navigator.clipboard.writeText(content);
+  });
+
+  // Create prefix input
+  const prefixInputElement = document.createElement('input');
+  prefixInputElement.placeholder = 'cmd --option [url] extra-arg';
+  prefixInputElement.className = 'outline-none flex-grow rounded border border-grey-200 p-1';
+  prefixInputElement.value = localStorage.getItem('pattern') || '';
+  prefixInputElement.addEventListener('input', (input) => {
+    writeListContent(input.target.value);
+    localStorage.setItem('pattern', input.target.value);
+  });
+
+  // Create action wrapper element
+  const actionWrapperElement = document.createElement('div');
+  actionWrapperElement.className = 'flex space-x-1';
+  actionWrapperElement.appendChild(resetButtonElement);
+  actionWrapperElement.appendChild(copyButtonElement);
+  actionWrapperElement.appendChild(prefixInputElement);
+
+  function writeListContent() {
+    listElement.innerHTML = urls.map((url) => {
+      if (!prefixInputElement.value) {
+        return `<li>${url}</li>`;
+      }
+
+      return `<li class="text-gray-400">${prefixInputElement.value.replace('[url]', `<span class="text-black">${url}</span>`)}</li>`;
+    }).join('');
+  }
+
   // Create url list
   const listElement = document.createElement('ol');
-  listElement.innerHTML = urls.map((url) => `<li>${url}</li>`).join('');
+  listElement.className = 'mt-1 ml-2 border-l-4 border-grey-300 pl-6 select-none list-decimal';
+  writeListContent();
   alertElement.parentElement.insertBefore(listElement, alertElement.nextSibling);
 
-  // Create reset button
-  const resetButton = document.createElement('button');
-  resetButton.innerHTML = '✗ Clear';
-  resetButton.addEventListener('click', () => {
-    localStorage.removeItem('urls');
-    listElement.innerHTML = '';
-    resetButton.style.display = listElement.style.display = 'none';
-  });
-  alertElement.parentElement.insertBefore(resetButton, listElement);
+  alertElement.parentElement.insertBefore(actionWrapperElement, listElement);
 })();
