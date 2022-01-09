0a16dd18d7affa49b8f7ca404214bbdabf3c525e
Thomas Vivien <thomas.yang.vivien@ik.me>
2022-01-09 12:47:11 +0100

    feat: use Vue3 to create the controls
    Remove Geetest auto resolve for Hyipstats

--- link-grabber.js
+++ link-grabber.js
@@ -9,6 +9,19 @@
 // ==/UserScript==
 
 (async () => {
+  async function getElement(selector) {
+    return new Promise((resolve) => {
+      const handler = setInterval(() => {
+        const element = document.querySelector(selector);
+
+        if (element) {
+          clearInterval(handler);
+          resolve(element);
+        }
+      }, 50);
+    });
+  }
+
   class PageObjectFactory {
     factory(url) {
       if (/.*journaldupirate.*/.exec(url)) {
@@ -36,10 +49,13 @@
       return document.querySelector('.content-body .alert a')?.href;
     }
 
-    injectElements() {
-      const anchor = document.querySelector('.content-body .alert');
-      anchor.parentElement.insertBefore(this.list, anchor);
-      anchor.parentElement.insertBefore(this.actions, this.list);
+    async getAppAnchor() {
+      const appAnchor = document.createElement('div');
+      appAnchor.id = 'app';
+
+      document.querySelector('.content-body .alert').appendChild(appAnchor);
+
+      return '#app';
     }
   }
 
@@ -58,10 +74,13 @@
       return document.querySelector('.page-content .alert a')?.href;
     }
 
-    injectElements() {
-      const anchor = document.querySelector('.page-content .alert');
-      anchor.parentElement.insertBefore(this.list, anchor);
-      anchor.parentElement.insertBefore(this.actions, this.list);
+    async getAppAnchor() {
+      const appAnchor = document.createElement('div');
+      appAnchor.id = 'app';
+
+      document.querySelector('.page-content .alert').appendChild(appAnchor);
+
+      return '#app';
     }
   }
 
@@ -71,51 +90,33 @@
       this.actions = document.createElement('div');
     }
 
-    async ready() {
-      if (this.getUrl()) {
-        const ad = await (new Promise((resolve) => {
-          const handler = setInterval(() => {
-            if (document.querySelector('.m-table-content')) {
-              clearInterval(handler);
-              resolve(document.querySelector('.m-table-content'));
-            }
-          }, 50);
-        }));
+    async removeAds() {
+      const selectors = ['.m-table-content', '.detn_stat1_block', '.detn_foot_ul', '#kt_content .d-none'];
 
-        ad.parentElement.removeChild(ad);
-
-        return Promise.resolve();
+      if (window.history.length === 1) {
+        selectors.push('.alert.alert-notice.alert-light-primary');
       }
 
-      const geetest = await (new Promise((resolve) => {
-        const handler = setInterval(() => {
-          if (document.querySelector('.geetest_holder')) {
-            clearInterval(handler);
-            resolve(document.querySelector('.geetest_holder'));
-          }
-        }, 50);
-      }));
+      const elements = await Promise.all(selectors.map(getElement));
 
-      geetest.dispatchEvent(new Event('click'));
-      document.querySelector('#get_link').submit();
+      elements.forEach((element) => element.parentElement.removeChild(element));
+    }
 
-      await (new Promise((resolve) => {
-        const handler = setInterval(() => {
-          if (document.querySelector('.geetest_success_radar_tip_content').innerHTML === 'Succeeded') {
-            clearInterval(handler);
-            resolve();
-          }
-        }, 50);
-      }));
+    async ready() {
+      await this.removeAds();
     }
 
     getUrl() {
       return document.querySelector('.main-body .alert-white a')?.href;
     }
 
-    injectElements() {
-      document.querySelector('.main-body').appendChild(this.list);
-      document.querySelector('.main-body').insertBefore(this.actions, this.list);
+    async getAppAnchor() {
+      const appAnchor = document.createElement('div');
+      appAnchor.id = 'app';
+
+      document.querySelector('.main-body').appendChild(appAnchor);
+
+      return '#app';
     }
   }
 
@@ -127,93 +128,134 @@
 
   await page.ready();
 
-  if (!page.getUrl()) {
-    console.warn('No link found in this page');
-    return;
-  }
+  await new Promise((resolve) => {
+    // Install TailwindCSS
+    const tailwindLinkElement = document.createElement('link');
+    tailwindLinkElement.href = 'https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css';
+    tailwindLinkElement.rel = 'stylesheet';
+    document.head.appendChild(tailwindLinkElement);
 
-  const urls = [];
-  function fetchUrls() {
-    const stringifiedUrls = JSON.stringify(urls);
-    const raw = localStorage.getItem('urls');
+    // Install Vue3
+    const vue3ScriptElement = document.createElement('script');
+    vue3ScriptElement.src = 'https://unpkg.com/vue@next';
+    document.head.appendChild(vue3ScriptElement);
 
-    if (stringifiedUrls !== raw) {
-      const store = JSON.parse(raw);
-
-      if (store && Array.isArray(store) && store.length) {
-        urls.splice(0, urls.length, ...store);
-        writeListContent();
+    const handle = setInterval(() => {
+      if (unsafeWindow.Vue) {
+        clearInterval(handle);
+        resolve();
       }
-    }
-  }
-
-  // Install TailwindCSS
-  const tailwindLinkElement = document.createElement('link');
-  tailwindLinkElement.href = 'https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css';
-  tailwindLinkElement.rel = 'stylesheet';
-  document.head.appendChild(tailwindLinkElement);
-
-  // Create reset button
-  const resetButtonElement = document.createElement('div');
-  resetButtonElement.className = 'cursor-pointer rounded border border-black border-red-600 hover:border-red-400 p-1 text-red-600 hover:text-red-400';
-  resetButtonElement.innerHTML = '<svg class="inline" style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" /></svg> Clear';
-  resetButtonElement.addEventListener('click', () => {
-    localStorage.removeItem('urls');
-    writeListContent();
-    page.actions.style.display = page.list.style.display = 'none';
+    }, 50);
   });
 
-  // Create copy button
-  const copyButtonElement = document.createElement('div');
-  copyButtonElement.className = 'cursor-pointer rounded border border-black border-blue-600 hover:border-blue-400 p-1 text-blue-600 hover:text-blue-400';
-  copyButtonElement.innerHTML = '<svg class="inline" style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M19,21H8V7H19M19,5H8A2,2 0 0,0 6,7V21A2,2 0 0,0 8,23H19A2,2 0 0,0 21,21V7A2,2 0 0,0 19,5M16,1H4A2,2 0 0,0 2,3V17H4V3H16V1Z" /></svg> Copy';
-  copyButtonElement.addEventListener('click', () => {
-    const content = prefixInputElement.value
-      ? urls.map((url) => prefixInputElement.value.replace('[url]', url)).join('\r\n')
-      : urls.join(' ');
+  const urlListComponent = Vue.createApp({
+    data() {
+      return {
+        urlList: [],
+      };
+    },
 
-    navigator.clipboard.writeText(`${content}\r\n`);
-  });
+    computed: {
+      urls: {
+        get() {
+          return this.urlList;
+        },
 
-  // Create prefix input
-  const prefixInputElement = document.createElement('input');
-  prefixInputElement.placeholder = 'cmd --option [url] extra-arg';
-  prefixInputElement.className = 'outline-none flex-grow rounded border border-grey-200 p-1';
-  prefixInputElement.value = localStorage.getItem('pattern') || '';
-  prefixInputElement.addEventListener('input', (input) => {
-    writeListContent(input.target.value);
-    localStorage.setItem('pattern', input.target.value);
-  });
+        set(urls) {
+          this.urlList = urls;
+          localStorage.setItem('urls', JSON.stringify(urls));
+        },
+      },
 
-  // Populate action wrapper element
-  page.actions.className = 'flex space-x-1';
-  page.actions.appendChild(resetButtonElement);
-  page.actions.appendChild(copyButtonElement);
-  page.actions.appendChild(prefixInputElement);
-
-  function writeListContent() {
-    page.list.innerHTML = urls.map((url) => {
-      if (!prefixInputElement.value) {
-        return `<li class="ml-8">${url}</li>`;
+      rawList() {
+        return JSON.stringify(this.urls);
       }
+    },
 
-      return `<li class="ml-8 text-gray-400">${prefixInputElement.value.replace('[url]', `<span class="text-gray-600">${url}</span>`)}</li>`;
-    }).join('');
-  }
+    mounted() {
+      this.syncUrls();
+      setInterval(() => this.syncUrls(), 500);
 
-  // Populate url list
-  page.list.className = 'my-1 ml-2 border-l-4 border-grey-300 pl-6 select-none list-decimal';
+      if (page.getUrl()) {
+        if (!this.urls.includes(page.getUrl())) {
+          this.urls = [...this.urls, page.getUrl()];
+        }
+      } else {
+        console.warn('No link found in this page');
+      }
+    },
 
-  page.injectElements();
+    methods: {
+      clear() {
+        this.urls = [];
+      },
 
-  fetchUrls();
+      copy() {
+        navigator.clipboard.writeText(this.urls.join('\r\n'));
+      },
 
-  // Filter duplicate urls
-  if (!urls.includes(page.getUrl())) {
-    urls.push(page.getUrl());
-    localStorage.setItem('urls', JSON.stringify(urls));
-    writeListContent();
-  }
+      syncUrls() {
+        const rawStoredList = localStorage.getItem('urls');
 
-  setInterval(fetchUrls, 500);
+        if (this.rawList !== rawStoredList) {
+          const storedList = JSON.parse(rawStoredList);
+
+          if (storedList && Array.isArray(storedList) && storedList.length) {
+            this.urls = storedList;
+          }
+        }
+      }
+    },
+
+    template: `
+      <div>
+        <div class="flex space-x-1">
+          <button
+            class="cursor-pointer rounded border border-black border-red-600 hover:border-red-400 p-1 text-red-600 hover:text-red-400 select-none"
+            @click="clear"
+          >
+            <svg
+              class="inline"
+              style="width:24px;height:24px"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z"
+              />
+            </svg> Clear
+          </button>
+
+          <button
+            class="cursor-pointer rounded border border-black border-blue-600 hover:border-blue-400 p-1 text-blue-600 hover:text-blue-400 select-none"
+            @click="copy"
+          >
+            <svg
+              class="inline"
+              style="width:24px;height:24px"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="M19,21H8V7H19M19,5H8A2,2 0 0,0 6,7V21A2,2 0 0,0 8,23H19A2,2 0 0,0 21,21V7A2,2 0 0,0 19,5M16,1H4A2,2 0 0,0 2,3V17H4V3H16V1Z"
+              />
+            </svg> Copy
+          </button>
+        </div>
+
+        <ol
+          v-if="urls.length"
+          class="my-1 ml-2 border-l-4 border-grey-300 pl-6 list-decimal"
+        >
+          <li
+            v-for="url in urls"
+            :key="url"
+            class="ml-8"
+          >{{ url }}</li>
+        </ol>
+      </div>
+    `,
+  });
+
+  urlListComponent.mount(await page.getAppAnchor());
 })();
