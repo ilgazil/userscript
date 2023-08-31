5e874b4ddb28c3c99022b54360ebf86e3b1298dc
Thomas Vivien <thomas+github@vivien.cloud>
2023-08-31 20:13:52 +0200

    feat: add darkino file filter

---
+++ darkino.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         Darkino filter
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Filter Darkino file versions
+// @author       Ilgazil
+// @match        https://www2.darkino.io/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=darkino.io
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  function filter(badge) {
+    const table = badge.closest('table.filament-tables-table');
+    const version = badge.innerText;
+
+    Array
+      .from(table.querySelectorAll('.badge.bg-success'))
+      .filter(({innerText}) => innerText != version)
+      .forEach((badge) => {
+        badge.closest('tr').style.display = 'none';
+      });
+  }
+
+  Array
+    .from(document.querySelectorAll('table.filament-tables-table .badge.bg-success'))
+    .forEach((badge) => {
+      badge.addEventListener('click', ({target}) => filter(target));
+      badge.style.cursor = 'pointer';
+    });
+})();
